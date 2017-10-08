e748bca21ca8a05e3643bcee0f891a957697d551
Zoltan Farkas <progfarkas@gmail.com>
2017-10-08 09:53:22 +0200

    finish positive test

--- spring4-mvc-gradle-xml/modules/core/src/test/java/com/mkyong/logic/service/module/PackageServiceTest.java
+++ spring4-mvc-gradle-xml/modules/core/src/test/java/com/mkyong/logic/service/module/PackageServiceTest.java
@@ -12,12 +12,14 @@ import com.mkyong.logic.repository.module.projectmodule.ProjectModule;
 import com.mkyong.logic.repository.module.projectmodule.ProjectModuleRepository;
 import com.mkyong.logic.repository.module.projectmodule.ProjectModuleRepositoryTestImpl;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PackageServiceTest {
 
     @Test
     public void positiveTest() {
+        // Given
         final PackageModule prodMySql = new PackageModule() {
             @Override
             public String getModuleName() {
@@ -29,19 +31,50 @@ public class PackageServiceTest {
                 return DatabaseType.MY_SQL;
             }
         };
+        final ProjectModule core = new ProjectModule() {
+            @Override
+            public String getModuleName() {
+                return "core";
+            }
+
+            @Override
+            public boolean hasDatabase() {
+                return true;
+            }
+        };
+        final DatabaseModule coreMySql = new DatabaseModule() {
+            @Override
+            public Class<? extends ProjectModule> getProjectModuleClass() {
+                return core.getClass();
+            }
+
+            @Override
+            public DatabaseType getDatabaseType() {
+                return DatabaseType.MY_SQL;
+            }
+        };
         PackageModuleRepository packageModuleRepository = new PackageModuleRepositoryTestImpl(ImmutableList.<PackageModule>builder()
                 .add(prodMySql)
                 .build());
         ProjectModuleRepository projectModuleRepository = new ProjectModuleRepositoryTestImpl(ImmutableList.<ProjectModule>builder()
-                // TODO
+                .add(core)
                 .build());
         DatabaseModuleRepository databaseModuleRepository = new DatabaseModuleRepositoryTestImpl(ImmutableList.<DatabaseModule>builder()
-                // TODO
+                .add(coreMySql)
                 .build());
         PackageService packageService = new PackageService(packageModuleRepository, projectModuleRepository, databaseModuleRepository);
 
+        // When
         Package pkg = packageService.getPackage();
-        // TODO
+
+        // Then
+        Assert.assertEquals("prodMySql", pkg.getName());
+        Assert.assertEquals(1, pkg.getProjectModules().size());
+        Assert.assertEquals("core", pkg.getProjectModules().get(0).getName());
+        Assert.assertNotNull(pkg.getProjectModules().get(0).getDatabaseModule());
+        Assert.assertEquals(DatabaseType.MY_SQL, pkg.getProjectModules().get(0).getDatabaseModule().getDatabaseType());
+        Assert.assertTrue(pkg.getDatabaseModule().isPresent());
+        Assert.assertEquals(DatabaseType.MY_SQL, pkg.getDatabaseModule().get().getDatabaseType());
     }
 
 }
